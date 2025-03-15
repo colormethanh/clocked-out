8e199f66d25db27d00e7c61c30e8edc2dc2d519d
Colormethanh <thnguyen1847@gmail.com>
2025-03-15 14:51:25 -0400

    added title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,37 +16,40 @@ export default function Home() {
   return (
     <div className="h-screen table w-full">
       <div className="h-100">
-        <main className="container px-5 py-24 mx-auto flex flex-wrap">
-          <InputsContainer>
-            {Object.keys(shiftSummary).map((key: string) => {
-              if (shiftSummary[key].fieldType === "input") {
-                return (
-                  <ShiftDataInput
-                    key={key}
-                    data={shiftSummary[key]}
-                    onInputChange={(event) =>
-                      handleShiftDataChange(event, shiftSummary[key].name)
-                    }
-                  />
-                );
-              } else {
-                return (
-                  <ShiftDataRange
-                    key={key}
-                    data={shiftSummary[key]}
-                    onInputChange={(value) =>
-                      handleShiftDataChange(value, shiftSummary[key].name)
-                    }
-                  />
-                );
-              }
-            })}
-          </InputsContainer>
-          <CalculationsContainer>
-            {Object.keys(calculations).map((key: string) => (
-              <ShiftCalculation key={key} calculation={calculations[key]} />
-            ))}
-          </CalculationsContainer>
+        <main className="container px-5 md:py-24 py-3 mx-auto ">
+          <h1 className="text-5xl font-bold my-3 title-font"> <span className="text-green-500">C</span>losed <span className="text-green-500">O</span>ut </h1>
+          <div className="flex flex-wrap">
+            <InputsContainer>
+              {Object.keys(shiftSummary).map((key: string) => {
+                if (shiftSummary[key].fieldType === "input") {
+                  return (
+                    <ShiftDataInput
+                      key={key}
+                      data={shiftSummary[key]}
+                      onInputChange={(event) =>
+                        handleShiftDataChange(event, shiftSummary[key].name)
+                      }
+                    />
+                  );
+                } else {
+                  return (
+                    <ShiftDataRange
+                      key={key}
+                      data={shiftSummary[key]}
+                      onInputChange={(value) =>
+                        handleShiftDataChange(value, shiftSummary[key].name)
+                      }
+                    />
+                  );
+                }
+              })}
+            </InputsContainer>
+            <CalculationsContainer>
+              {Object.keys(calculations).map((key: string) => (
+                <ShiftCalculation key={key} calculation={calculations[key]} />
+              ))}
+            </CalculationsContainer>
+          </div>
         </main>
       </div>
     </div>
